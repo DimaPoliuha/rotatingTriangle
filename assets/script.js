caa2111bf34148b0b6b7d6cee26852a84805fee4
DmitryPoliuha <dmitry.polyuha@gmail.com>
2018-03-27 10:57:13 +0300

    var -> let

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var canvas = document.getElementById( 'canvas' );
-var ctx = canvas.getContext( '2d' );
+let canvas = document.getElementById( 'canvas' );
+let ctx = canvas.getContext( '2d' );
 
 canvas.width = 500;
 canvas.height = 500;
 
-var triangleBaseSide = 70;
-var triangleSide = 100;
+let triangleBaseSide = 70;
+let triangleSide = 100;
 triangleSide = (( triangleSide * 2 ) > triangleBaseSide ) ? triangleSide : ( triangleBaseSide / 2 + 1 );
 
-var documentCoordinates = {
+let documentCoordinates = {
     centerDocumentPositionX: ( window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth ) / 2,
     centerDocumentPositionY: ( window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight ) / 2,
 };
-var canvasCoordinates = {
+let canvasCoordinates = {
     canvasCenterX: canvas.width / 2,
     canvasCenterY: canvas.height / 2,
     canvasPositionLeft: documentCoordinates.centerDocumentPositionX - canvas.width / 2,
@@ -32,12 +32,12 @@ window.onload = function () {
 function rotateTriangle( event ){
     ctx.save();
     ctx.translate( canvasCoordinates.canvasCenterX, canvasCoordinates.canvasCenterY );
-    var triandleTranslation = {
+    let triandleTranslation = {
         dx: event.pageX - documentCoordinates.centerDocumentPositionX,
         dy: event.pageY - documentCoordinates.centerDocumentPositionY
     };
     // arctg(y/x)  - angle of rotation (+ PI / 2) - rotate our triangle for correct starting position;
-    var rotatingAngle = Math.atan( triandleTranslation.dy / triandleTranslation.dx ) + Math.PI / 2;
+    let rotatingAngle = Math.atan( triandleTranslation.dy / triandleTranslation.dx ) + Math.PI / 2;
     // quarter check
     if ( triandleTranslation.dx < 0 ){
         rotatingAngle += Math.PI;
@@ -51,8 +51,8 @@ function rotateTriangle( event ){
 function drawTriangle(){
     ctx.clearRect( 0, 0, canvas.width, canvas.height );
     // intersection of medians is the center of mass of triangle
-    var median = Math.sqrt( Math.pow( triangleSide, 2 ) - Math.pow( triangleBaseSide, 2 ) / 4 );
-    var triangleCoordinates = {
+    let median = Math.sqrt( Math.pow( triangleSide, 2 ) - Math.pow( triangleBaseSide, 2 ) / 4 );
+    let triangleCoordinates = {
         trianglePointTopX: canvasCoordinates.canvasCenterX,
         trianglePointTopY: canvasCoordinates.canvasCenterY - median * 2 / 3,
         trianglePointBottomLeftX: canvasCoordinates.canvasCenterX - triangleBaseSide / 2,
